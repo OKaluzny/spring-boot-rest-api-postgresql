8df22d1b10de04ae593157b8db2a9bca9250407c
Oleg Kaluzny <cryoengineer@gmail.com>
2019-11-21 13:42:51 +0200

    added logging

--- src/main/java/com/kaluzny/web/BookRestController.java
+++ src/main/java/com/kaluzny/web/BookRestController.java
@@ -3,6 +3,7 @@ package com.kaluzny.web;
 import com.kaluzny.domain.Book;
 import com.kaluzny.domain.BookRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -12,57 +13,79 @@ import java.util.Collection;
 @RestController
 @RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 @RequiredArgsConstructor
+@Slf4j
 public class BookRestController {
 
     private final BookRepository repository;
 
     @PostMapping("/books")
     @ResponseStatus(HttpStatus.CREATED)
-    public Book addBook(@RequestBody Book book) {
-        return repository.save(book);
+    public Book saveBook(@RequestBody Book book) {
+        log.info("saveBook() - start: book = {}", book);
+        Book savedBook = repository.save(book);
+        log.info("saveBook() - end: savedBook = {}", savedBook.getId());
+        return savedBook;
     }
 
     @GetMapping("/books")
     @ResponseStatus(HttpStatus.OK)
     public Collection<Book> getAllBooks() {
-        return repository.findAll();
+        log.info("getAllBooks() - start");
+        Collection<Book> collection = repository.findAll();
+        log.info("getAllBooks() - end");
+        return collection;
     }
 
     @GetMapping("/books/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Book getBookWithId(@PathVariable Long id) {
-        return repository.findOne(id);
+    public Book getBookById(@PathVariable Long id) {
+        log.info("getBookById() - start: id = {}", id);
+        Book receivedBook = repository.findOne(id);
+        log.info("getBookById() - end: book = {}", receivedBook.getId());
+        return receivedBook;
     }
 
     @GetMapping(value = "/books", params = {"name"})
     @ResponseStatus(HttpStatus.OK)
-    public Collection<Book> findBookWithName(@RequestParam(value = "name") String name) {
-        return repository.findByName(name);
+    public Collection<Book> findBookByName(@RequestParam(value = "name") String name) {
+        log.info("findBookByName() - start: name = {}", name);
+        Collection<Book> collection = repository.findByName(name);
+        log.info("findBookByName() - end: collection = {}", collection);
+        return collection;
     }
 
     @PutMapping("/books/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Book updateBookFromDB(@PathVariable("id") long id, @RequestBody Book book) {
-        return repository.save(putBook(id, book));
+    public Book refreshBook(@PathVariable("id") long id, @RequestBody Book book) {
+        log.info("refreshBook() - start: id = {}, book = {}", id, book);
+        Book updatedBook = repository.save(putBook(id, book));
+        log.info("refreshBook() - end: updatedBook = {}", updatedBook);
+        return updatedBook;
     }
 
     @DeleteMapping("/books/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteBookWithId(@PathVariable Long id) {
+    public void removeBookById(@PathVariable Long id) {
+        log.info("removeBookById() - start: id = {}", id);
         repository.delete(id);
+        log.info("removeBookById() - end: id = {}", id);
     }
 
     @DeleteMapping("/books")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteAllBooks() {
+    public void removeAllBooks() {
+        log.info("removeAllBooks() - start");
         repository.deleteAll();
+        log.info("removeAllBooks() - end");
     }
 
     private Book putBook(long id, Book existingBook) {
+        log.info("putBook() - start: id = {} existingBook = {}", id, existingBook);
         Book putBook = repository.findOne(id);
         putBook.setName(existingBook.getName());
         putBook.setDescription(existingBook.getDescription());
         putBook.setTags(existingBook.getTags());
+        log.info("putBook() - end: putBook = {}", putBook);
         return putBook;
     }
 }
